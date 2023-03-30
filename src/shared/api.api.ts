98a0726c290502d3564d0401d239e519c088f09e
v.skoropey <v.skoropey@securitycode.ru>
2023-03-30 13:42:19 +0300

    updated loki

--- src/shared/api.api.ts
+++ src/shared/api.api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from './const/localstorage';
 
-
 export const $api = axios.create({
     baseURL: __API,
     headers: {
--- src/widgets/SideBar/ui/SideBar/ui/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/ui/SideBar.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { SideBar } from 'widgets/SideBar';
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
